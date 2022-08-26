cbfaeec2329b0f280e35ef861a5b774d78dae661
ariel <anbp_1993@hotmail.com>
2022-08-25 21:28:54 -0500

    examen 3

---
+++ Los Joaoa/ArielBoloña_examen3.js
@@ -0,0 +1,16 @@
+ var matriz1;
+ var matriz2 ;
+
+ var matriz1=[3 , 2 , 1 , 1 , 2 , 3 , 2 , 3 , 1];
+ var matriz2=[1 , 1 , 2 , 2 , 1 , 1 , 1 , 2 , 1 ] ;
+ 
+ console.log (matriz1)
+ console.log (matriz2)
+
+
+ var producto = (matriz1*matriz2);
+
+ console.log (producto)
+ 
+var suma = ( matriz1+matriz1 ) ;
+console.log (suma)
